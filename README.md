7f03420ed701e6240a50eee8febe7ac62e32b2d8
jalik <karl.stein.pro@gmail.com>
2018-03-01 16:22:17 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# js-extend
+
+A simple function to merge objects.
+
+## Introduction
+
+Useful when you need to merge two or more objects into one.
+
+**This library is tested with unit tests.**
+
+## Merging objects
+
+In this example, all objects are merged into the first object, so the first object is modified, if you don't want to modify the object, pass en empty object `{}` as the first argument.
+
+Note that objects are merged based on their order, so the 2nd object is merged in the first, then the 3rd is merged in the first, and so on...
+Keep in mind that first objects may be overridden by following objects.
+
+```js
+import {extend} from "@jalik/extend";
+
+const coldColors = {
+    blue: "#0000FF",
+    green: "#00FF00"
+};
+
+const hotColors = {
+    red: "#FF0000",
+    yellow: "#FFF000"
+};
+
+const customColors = {
+    cyan: "#5BF8FF",
+    pink: "#FF4CFB"
+};
+
+// Merge all colors in a new object
+const mixedColors = extend({}, hotColors, coldColors, customColors);
+```
+
+## Merging objects recursively
+
+In the previous example, all objects were flat, but it works also with deep objects and nested attributes.
+
+```js
+import {extendRecursively} from "@jalik/extend";
+
+const colorSet1 = {
+    cold: {
+        blue: "#0000FF",
+        green: "#00FF00"
+    },
+    hot: {
+        red: "#FF0000",
+        yellow: "#FFF000"
+    },
+    custom: {
+      cyan: "#5BF8FF",
+      pink: "#FF4CFB"
+    }
+};
+
+const colorSet2 = {
+    cold: {
+        blue: "#0011AA",
+        green: "#00AA11"
+    },
+    hot: {
+        red: "#AA0011",
+        yellow: "#AAA111"
+    }
+};
+
+// Merge all colors in a new object
+const mixedColors = extendRecursively({}, colorSet1, colorSet2);
+```
+
+## Changelog
+
+### v1.0.0
+- First public release
+
+## License
+
+The code is released under the [MIT License](http://www.opensource.org/licenses/MIT).
---
+++ test/index.test.js
@@ -0,0 +1,73 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Karl STEIN
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import {extend, extendRecursively} from "../src";
+
+describe("extend()", () => {
+
+    it(`extend(null, null) should return null`, () => {
+        expect(extend(null, null)).toEqual(null);
+    });
+
+    it(`extend(null, {a: true}) should return an object`, () => {
+        expect(extend(null, {a: true})).toEqual({a: true});
+    });
+
+    it(`extend({a: true}, null) should return an object`, () => {
+        expect(extend({a: true}, null)).toEqual({a: true});
+    });
+
+    it(`extend({a: true}, {a: false}) should merge objects`, () => {
+        expect(extend({a: true}, {a: false})).toEqual({a: false});
+        expect(extend({a: true}, {b: false})).toEqual({a: true, b: false});
+    });
+
+    it(`extend({a: true}, {b: {c: false}}) should merge objects non recursively`, () => {
+        expect(extend({a: true, b: {d: 0}}, {b: {c: false}})).toEqual({a: true, b: {c: false}});
+    });
+});
+
+describe("extendRecursively()", () => {
+
+    it(`extendRecursively(null, null) should return null`, () => {
+        expect(extendRecursively(null, null)).toEqual(null);
+    });
+
+    it(`extendRecursively(null, {a: true}) should return an object`, () => {
+        expect(extendRecursively(null, {a: true})).toEqual({a: true});
+    });
+
+    it(`extendRecursively({a: true}, null) should return an object`, () => {
+        expect(extendRecursively({a: true}, null)).toEqual({a: true});
+    });
+
+    it(`extendRecursively({a: true}, {a: false}) should merge objects`, () => {
+        expect(extendRecursively({a: true}, {a: false})).toEqual({a: false});
+        expect(extendRecursively({a: true}, {b: false})).toEqual({a: true, b: false});
+    });
+
+    it(`extendRecursively({a: true}, {b: {c: false}}) should merge objects recursively`, () => {
+        expect(extendRecursively({a: true, b: {d: 0}}, {b: {c: false}})).toEqual({a: true, b: {d: 0, c: false}});
+    });
+});
