ca492bd66af8c8c61a1dfab351b7fae2e68db420
jalik <karl.stein.pro@gmail.com>
2018-10-12 16:16:11 -1000

    Rewrites unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -27,93 +27,88 @@ import extend from '../src';
 it('extend(null, null) should return null', () => {
   const a = null;
   const b = null;
-  const result = null;
-  expect(extend(a, b)).toEqual(result);
+  expect(extend(a, b)).toEqual(null);
 });
 
-it('extend(null, {a: true}) should return an object', () => {
-  const a = null;
-  const b = { a: true };
-  const result = { a: true };
-  expect(extend(a, b)).toEqual(result);
-});
-
-it('extend(undefined, {a: true}) should return an object', () => {
+it('extend(undefined, undefined) should return undefined', () => {
   const a = undefined;
-  const b = { a: true };
-  const result = { a: true };
-  expect(extend(a, b)).toEqual(result);
-});
-
-it('extend({a: true}, null) should return an object', () => {
-  const a = { a: true };
-  const b = null;
-  const result = { a: true };
-  expect(extend(a, b)).toEqual(result);
-});
-
-it('extend({ a: 0 }, undefined, { b: 1 }) should return { a: 0, b: 1 }', () => {
-  const a = { a: 0 };
   const b = undefined;
-  const c = { b: 1 };
-  const result = { a: 0, b: 1 };
-  expect(extend(a, b, c)).toEqual(result);
+  expect(extend(a, b)).toEqual(undefined);
 });
 
-it('extend({a: true}, {a: false}) should merge objects', () => {
-  const a = { a: true };
-  const b = { a: false, b: true };
-  const result = { a: false, b: true };
-  expect(extend(a, b)).toEqual(result);
-});
+// Objects
 
-it('extend({a: true}, {b: {c: false}}) should merge objects non recursively', () => {
-  const a = { a: true, b: { d: 0 } };
-  const b = { b: { c: false } };
-  const result = { a: true, b: { c: false } };
-  expect(extend(a, b)).toEqual(result);
-});
-
-it('extend([0, 1], [undefined, 7]) should return [0, 7]', () => {
-  const a = [0, 1];
-  const b = [undefined, 7];
-  const result = [0, 7];
-  expect(extend(a, b)).toEqual(result);
-});
-
-it('extend([0, 1], [undefined, 7]) should return [0, 7]', () => {
-  const a = [2, 4, null, 6];
-  const b = [1, 3, 5, undefined];
-  const result = [1, 3, 5, 6];
-  expect(extend(a, b)).toEqual(result);
-});
-
-it('extend({}, [10, 20]) should return { 0: 10, 1: 20 }', () => {
-  const a = {};
-  const b = [10, 20];
-  const result = { 0: 10, 1: 20 };
-  expect(extend(a, b)).toEqual(result);
-});
-
-it('extend(null, [1], undefined, [2]) should return [2]', () => {
+it('extend() should replace null values', () => {
   const a = null;
-  const b = [1];
-  const c = undefined;
-  const d = [2];
-  const result = [2];
-  expect(extend(a, b, c, d)).toEqual(result);
+  const b = { a: 1 };
+  expect(extend(a, b)).toEqual(b);
 });
 
-it('extend({ length: numbers.length }, [1, 3, 3, 7]) should return { length, ...numbers }', () => {
-  const numbers = [1, 3, 3, 7];
-  const a = { length: numbers.length };
-  const b = [1, 3, 3, 7];
-  const result = {
-    length: numbers.length,
-    0: 1,
-    1: 3,
-    2: 3,
-    3: 7,
-  };
-  expect(extend(a, b)).toEqual(result);
+it('extend() should replace undefined values', () => {
+  const a = undefined;
+  const b = { a: 1 };
+  expect(extend(a, b)).toEqual(b);
+});
+
+it('extend() should ignore null values', () => {
+  const a = { a: 1 };
+  const b = null;
+  expect(extend(a, b)).toEqual(a);
+});
+
+it('extend() should ignore undefined values', () => {
+  const a = { a: 1 };
+  const b = undefined;
+  expect(extend(a, b)).toEqual(a);
+});
+
+it('extend() should replace existing attributes', () => {
+  const a = { a: 1 };
+  const b = { a: 2 };
+  expect(extend(a, b)).toEqual(b);
+});
+
+it('extend() should merge new attributes', () => {
+  const a = { a: 1 };
+  const b = { b: 2 };
+  const r = { a: 1, b: 2 };
+  expect(extend(a, b)).toEqual(r);
+});
+
+it('extend() should merge objects non recursively', () => {
+  const a = { a: { b: 1, c: 3 }, d: 4 };
+  const b = { a: { b: 2 } };
+  const r = { a: { b: 2 }, d: 4 };
+  expect(extend(a, b)).toEqual(r);
+});
+
+// Arrays
+
+it('extend() should merge arrays by replacing index value', () => {
+  const a = [0, 1];
+  const b = [0, 2];
+  expect(extend(a, b)).toEqual(b);
+});
+
+it('extend() should ignore undefined values when merging arrays', () => {
+  const a = [0, 1];
+  const b = [0, undefined];
+  const r = [0, 1];
+  expect(extend(a, b)).toEqual(r);
+});
+
+it('extend() should merge arrays non recursively', () => {
+  const a = [1, [2, [3]]];
+  const b = [undefined, [4, [undefined, 5], 6], 7];
+  const r = [1, [4, [undefined, 5], 6], 7];
+  expect(extend(a, b)).toEqual(r);
+});
+
+// Arrays to Object
+
+it('extend() should merge arrays into objects', () => {
+  const a = {};
+  const b = [1];
+  const r = { 0: 1 };
+  expect(extend(a, b)).toEqual(r);
 });
