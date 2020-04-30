38b54efd7465b5f521db2216aebd8e25ae749a14
Karl Stein <karl.stein.pro@gmail.com>
2020-04-29 14:50:18 -1000

    Reorganizes tests

--- test/extend.test.js
+++ test/extend.test.js
@@ -24,95 +24,134 @@
 
 import extend from '../src/extend';
 
-it('extend(null, null) should return null', () => {
-  expect(extend(null, null)).toEqual(null);
+describe('extend(null, null)', () => {
+  it('should return null', () => {
+    expect(extend(null, null)).toEqual(null);
+  });
 });
 
-it('extend(undefined, undefined) should return undefined', () => {
-  expect(extend(undefined, undefined)).toEqual(undefined);
+describe('extend(undefined, undefined)', () => {
+  it('should return undefined', () => {
+    expect(extend(undefined, undefined)).toEqual(undefined);
+  });
 });
 
 // Objects
 
-it('extend() should replace null values', () => {
-  const b = { a: 1 };
-  expect(extend(null, b)).toEqual(b);
+describe('extend(null, object)', () => {
+  it('should return an object', () => {
+    const b = { a: 1 };
+    expect(extend(null, b)).toEqual(b);
+  });
 });
 
-it('extend() should replace undefined values', () => {
-  const b = { a: 1 };
-  expect(extend(undefined, b)).toEqual(b);
+describe('extend(undefined, object)', () => {
+  it('should return an object', () => {
+    const b = { a: 1 };
+    expect(extend(undefined, b)).toEqual(b);
+  });
 });
 
-it('extend() should ignore null values', () => {
-  const a = { a: 1 };
-  expect(extend(a, null)).toEqual(a);
+describe('extend(object, null)', () => {
+  it('should return an object', () => {
+    const a = { a: 1 };
+    expect(extend(a, null)).toEqual(a);
+  });
 });
 
-it('extend() should ignore undefined values', () => {
-  const a = { a: 1 };
-  expect(extend(a, undefined)).toEqual(a);
+describe('extend(object, undefined)', () => {
+  it('should return an object', () => {
+    const a = { a: 1 };
+    expect(extend(a, undefined)).toEqual(a);
+  });
 });
 
-it('extend() should replace existing attributes', () => {
-  const a = { a: 1 };
-  const b = { a: 2 };
-  expect(extend(a, b)).toEqual(b);
+describe('extend(object, object)', () => {
+  it('should replace existing attributes', () => {
+    const a = { a: 1 };
+    const b = { a: 2 };
+    expect(extend(a, b)).toEqual(b);
+  });
 });
 
-it('extend() should merge new attributes', () => {
-  const a = { a: 1 };
-  const b = { b: 2 };
-  const r = { a: 1, b: 2 };
-  expect(extend(a, b)).toEqual(r);
+describe('extend(object, object)', () => {
+  it('should merge new attributes', () => {
+    const a = { a: 1 };
+    const b = { b: 2 };
+    const r = { a: 1, b: 2 };
+    expect(extend(a, b)).toEqual(r);
+  });
 });
 
-it('extend() should merge objects non recursively', () => {
-  const a = { a: { b: 1, c: 3 }, d: 4 };
-  const b = { a: { b: 2 } };
-  const r = { a: { b: 2 }, d: 4 };
-  expect(extend(a, b)).toEqual(r);
+describe('extend(object, object)', () => {
+  it('should merge objects non recursively', () => {
+    const a = { a: { b: 1, c: 3 }, d: 4 };
+    const b = { a: { b: 2 } };
+    const r = { a: { b: 2 }, d: 4 };
+    expect(extend(a, b)).toEqual(r);
+  });
 });
 
-it('should not modify input array', () => {
-  const a = { array: null };
-  const b = { array: [1, 2, 3] };
-  expect(extend(a, b).array).not.toBe(b.array);
+describe('extend(object, object)', () => {
+  it('should not modify input array', () => {
+    const a = { array: null };
+    const b = { array: [1, 2, 3] };
+    expect(extend(a, b).array).not.toBe(b.array);
+  });
 });
 
 // Arrays
 
-it('extend() should merge arrays by replacing index value', () => {
-  const a = [0, 1];
-  const b = [0, 2];
-  expect(extend(a, b)).toEqual(b);
+describe('extend(array, array)', () => {
+  it('should merge arrays by replacing index value', () => {
+    const a = [0, 1];
+    const b = [0, 2];
+    expect(extend(a, b)).toStrictEqual(b);
+  });
 });
 
-it('extend() should ignore undefined values when merging arrays', () => {
-  const a = [0, 1];
-  const b = [0, undefined];
-  const r = [0, 1];
-  expect(extend(a, b)).toEqual(r);
+describe('extend(array, array)', () => {
+  it('should ignore undefined values when merging arrays', () => {
+    const a = [0, 1];
+    const b = [0, undefined];
+    const r = [0, 1];
+    expect(extend(a, b)).toStrictEqual(r);
+  });
 });
 
-it('extend() should merge arrays non recursively', () => {
-  const a = [1, [2, [3]]];
-  const b = [undefined, [4, [undefined, 5], 6], 7];
-  const r = [1, [4, [undefined, 5], 6], 7];
-  expect(extend(a, b)).toEqual(r);
+describe('extend(array, array)', () => {
+  it('should not ignore null values when merging arrays', () => {
+    const a = [0, 1];
+    const b = [0, null];
+    const r = [0, null];
+    expect(extend(a, b)).toStrictEqual(r);
+  });
 });
 
-it('should not modify input array', () => {
-  const a = null;
-  const b = [1, 2, 3];
-  expect(extend(a, b)).not.toBe(b);
+describe('extend(array, array)', () => {
+  it('should merge arrays non recursively', () => {
+    const a = [1, [2, [3]]];
+    const b = [undefined, [4, [undefined, 5], 6], 7];
+    const r = [1, [4, [undefined, 5], 6], 7];
+    expect(extend(a, b)).toStrictEqual(r);
+  });
+});
+
+describe('extend(any, array)', () => {
+  it('should not modify input array', () => {
+    const a = null;
+    const b = [1, 2, 3];
+    expect(extend(a, b)).not.toBe(b);
+  });
 });
 
 // Arrays to Object
 
-it('extend() should merge arrays into objects', () => {
-  const a = {};
-  const b = [1];
-  const r = { 0: 1 };
-  expect(extend(a, b)).toEqual(r);
+describe('extend(object, array)', () => {
+  it('should merge array into object', () => {
+    const a = {};
+    const b = [1];
+    const r = { 0: 1 };
+    expect(extend(a, b)).toEqual(r);
+  });
 });
